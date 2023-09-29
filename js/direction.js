7e72921d97e64c3c05b3140d9bf423cb60050a23
hemilynara <hemilynara@gmail.com>
2023-09-29 16:01:54 -0300

    Ajustando a página EasyCarros

---
+++ js/direction.js
@@ -0,0 +1,4 @@
+ // Função para redirecionar após 5 segundos
+ setTimeout(function () {
+    window.location.href = "sobreinicial.html"; // Substitua "pagina2.html" pelo URL da próxima página
+}, 2000); // 2000 milissegundos = 2 segundos
